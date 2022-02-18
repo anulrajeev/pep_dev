086236e036f3a0bfddcbd9d08faaf07a3f24d2fe
anulrajeev <anulrajeev@gmail.com>
2022-02-18 11:49:28 +0530

    18_feb

--- 2_Javascript/File_System_Organiser/activity/commands/organize.js
+++ 2_Javascript/File_System_Organiser/activity/commands/organize.js
@@ -1,5 +1,72 @@
-function organizeFn() {
+const fs = require("fs");
+const path = require("path");
+let utility = {
+    media: ['mp4','mkv','mp3'],
+    archives: ['zip','7z','rar','tar','gz','ar','iso','xz'],
+    documents: ['docx','doc','pdf','xlsx','xls','odt','ods','odp','odg','odf','txt','ps','tex'],
+    app: ['exe','dmg','pkg','deb']
+}
+function organizeFn(src) {
     console.log("Organizing your folder");
+    if(src==undefined )
+        src=process.cwd();
+    
+    let dest = checkDest(src);
+    organizeHelper(src, dest);
+}
+
+function checkDest(src)
+{
+    let dest=path.join(src, "organizedFolder");
+    if(fs.existsSync(dest)==false)
+    {
+        console.log("creating folder");
+        fs.mkdirSync(dest);
+
+    }
+    return dest;
+}
+function checkFileOrFolder(path) {
+    let isFile = fs.lstatSync(path).isFile();
+    return isFile;
+}
+
+
+function getCategory(src)
+{
+    // let extension = path.extname(src);or
+    let extension = src.split(".")[1];
+    for(let key in utility)
+    {
+        let valueArr = utility[key];
+        for(let i =0; i<valueArr.length;i++)
+        {
+            if(extension == valueArr[i])
+                return key;
+
+        }
+    }
+    return "others"; // if path doesn't fall under any category
+}
+
+function organizeHelper(src, dest)
+{
+    console.log("organizing");
+    let isFile = checkFileOrFolder(src);
+    if (isFile == true) {  // Base case
+        
+        let category = getCategory(src);
+        console.log(path.basename(src), " => ", category);
+    } else { 
+        let childrens = fs.readdirSync(src); 
+        console.log(src);
+        for (let i = 0;i < childrens.length;i++) {
+            let child = childrens[i]; 
+            let childPath = path.join(src, child);
+            organizeHelper(childPath, dest);
+            
+        }
+    }
 }
 
 module.exports = {
--- 2_Javascript/File_System_Organiser/activity/commands/view.js
+++ 2_Javascript/File_System_Organiser/activity/commands/view.js
@@ -3,7 +3,10 @@ let path = require("path");
 
 function viewFn(src, mode) {
     console.log("Viewing your folder");
-    viewHelperFlat(src);
+    if(mode=="flat")
+       viewHelperFlat(src);
+    else   
+       viewHelperTree(src,"");
 }
 
 function checkFileOrFolder(path) {
@@ -18,13 +21,34 @@ function viewHelperFlat(src) {
         console.log(src, "*");
     } else { // Folder => Folder1, Files
         let childrens = fs.readdirSync(src); // Read content Immediate Level childrens
-        
+        console.log(src);
         for (let i = 0;i < childrens.length;i++) {
             let child = childrens[i]; // Not a complete path, just file or folder name
             let childPath = path.join(src, child);
-            console.log(childPath);
+            // console.log(src);
 
             // Faith ??
+            viewHelperFlat(childPath);
+            
+        }
+    }
+}
+
+function viewHelperTree(src, indent) {
+    // Is it a file or folder
+    let isFile = checkFileOrFolder(src);
+    if (isFile == true) {  // Base case
+        console.log(indent, path.basename(src), "*");
+    } else { // Folder => Folder1, Files
+        let childrens = fs.readdirSync(src); // Read content Immediate Level childrens
+        console.log(indent, path.basename(src));
+        for (let i = 0;i < childrens.length;i++) {
+            let child = childrens[i]; // Not a complete path, just file or folder name
+            let childPath = path.join(src, child);
+            // console.log(src);
+
+            // Faith ??
+            viewHelperTree(childPath, indent+"\t");
             
         }
     }
