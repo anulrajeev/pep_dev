b784831130ca23189be05cfb0b49277b797920a3
anulrajeev <anulrajeev@gmail.com>
2022-03-18 09:50:01 +0530

    update

---
+++ 2_Javascript/JS_activities/let_var_const.js
@@ -0,0 +1,15 @@
+var real = function real(){
+    console.log("Suren");
+}
+real();
+
+real = function real(){
+    console.log("lokesh");
+}
+real();
+
+
+real = function real(){
+    console.log("Sanjay");
+}
+real();
