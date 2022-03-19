c737c5500a53aabda9b25c55bfbd19229f4c964e
anulrajeev <anulrajeev@gmail.com>
2022-03-19 09:45:01 +0530

    update

--- 2_Javascript/web scrapping/raw/request-response.js
+++ 2_Javascript/web scrapping/raw/request-response.js
@@ -1,1 +1,15 @@
-git
+const request = require("request")
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+request("https://www.google.co.in/", cb);
+function cb(err, res, body)
+{
+    if(err)
+       console.log("Some error  :  \n", err);
+    else
+    {
+        const dom = new JSDOM(body);
+        console.log(dom.window.document.querySelector(".NKcBbd").textContent); 
+        // console.log(body);
+    }
+}
